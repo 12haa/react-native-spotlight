4ae7c724f1ba43b644ab939ce37baf508ffb4113
12haa <12hadoto@gmail.com>
2025-04-03 20:00:59 +0330

    Finish Adding Post Page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -27,7 +27,10 @@ export default function Index() {
           <Ionicons name="log-out-outline" size={28} color={COLORS.white} />
         </TouchableOpacity>
       </View>
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 60 }}
+      >
         <ScrollView showsVerticalScrollIndicator={false} horizontal style={styles.storiesContainer}>
           {/* Feed */}
           {mockUsers.map((user) => (
--- components/Posts.tsx
+++ components/Posts.tsx
@@ -4,9 +4,11 @@ import { styles } from '@/styles/feed/feed.styles';
 import { Link } from 'expo-router';
 import { Image } from 'expo-image';
 import { Post } from '@/types/posts';
+import { Ionicons } from '@expo/vector-icons';
+import COLORS from '@/constants/theme';
 
 export default function Posts({ posts }: { posts: Post }) {
-  console.log(posts);
+
   return (
     <View style={styles.post}>
       {/* Post Header */}
@@ -23,6 +25,44 @@ export default function Posts({ posts }: { posts: Post }) {
             <Text style={styles.postUsername}>{posts.author}</Text>
           </TouchableOpacity>
         </Link>
+        <TouchableOpacity>
+          <Ionicons name="ellipsis-horizontal" size={20} color={COLORS.white} />
+          {/* <Ionicons name="ellipsis-horizontal" size={20} color={COLORS.white} /> */}
+        </TouchableOpacity>
+      </View>
+      {/* Post Image */}
+      <Image
+        source={posts.imageUrl}
+        style={styles.postImage}
+        transition={200}
+        contentFit="cover"
+        cachePolicy="memory-disk"
+      />
+      {/* Post Actions */}
+      <View style={styles.postActions}>
+        <View style={styles.postActionsLeft}>
+          <TouchableOpacity>
+            <Ionicons name="heart-outline" size={24} color={COLORS.primary} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="chatbubble-outline" size={22} color={COLORS.white} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="bookmark-outline" size={22} color={COLORS.white} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      {/* Post Info */}
+      <View style={styles.postInfo}>
+        <Text style={styles.likesText}>Be the first to like</Text>
+        {posts.caption && (
+          <View style={styles.captionContainer}>
+            <Text style={styles.captionUsername}>{posts.author}</Text>
+            <Text style={styles.captionText}>{posts.caption}</Text>
+          </View>
+        )}
+        <TouchableOpacity style={styles.commentsText}>View All Comments</TouchableOpacity>
+        <Text style={styles.timeAgo}>2 hours ago</Text>
       </View>
     </View>
   );
